5f932d0806cff7104c3f0bfb5e0575ef9c51acb8
FABallemand <allemand.fabien@orange.fr>
2023-02-07 12:28:21 +0100

    Test

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split, cross_val_score, cross_val_predict
 
-from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import GaussianNB, ComplementNB
 from sklearn.neighbors import KNeighborsClassifier
 
 import utils
@@ -13,11 +13,12 @@ train_input, train_output = utils.loadTrainingData(remove_id=True, remove_capuch
 
 # Create model
 # clf = GaussianNB()
-clf = KNeighborsClassifier(n_neighbors=3) # weights="distance"
+# clf = KNeighborsClassifier(n_neighbors=3) # weights="distance"
+clf = ComplementNB()
 
 # y_pred = cross_val_predict(clf, train_input, train_output['result'], cv=5)
 # print(y_pred)
 
 # Evaluate model
-utils.modelEvaluation(clf, train_input, train_output, balance_classes=True, model_name="Test Classifier", fig_name="test")
+utils.modelEvaluation(clf, train_input, train_output, balance_classes=False, model_name="Test Classifier", fig_name="test")
 
